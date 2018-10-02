632391ca2a7cd0b8e991841f6fc8c97a433f96af
Leo Torres <leohtor@gmail.com>
2018-10-01 20:57:45 -0700

    Added service worker back.

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,19 +23,19 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: 'Leo-T',
-    //     short_name: 'Leo-T',
-    //     start_url: '/',
-    //     // background_color: '#663399',
-    //     // theme_color: '#663399',
-    //     display: 'minimal-ui',
-    //     icon: 'src/images/fav-icon.png', // This path is relative to the root of the site.
-    //   },
-    // },
-    // 'gatsby-plugin-offline',
-    'gatsby-plugin-remove-serviceworker',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Leo-T',
+        short_name: 'Leo-T',
+        start_url: '/',
+        // background_color: '#663399',
+        // theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/fav-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
+    // 'gatsby-plugin-remove-serviceworker',
   ],
 }
